8615a2e009d9ab17d75df7b53dcf1df2fe58a08d
iJustinn <ijustinzhong@gmail.com>
2024-11-04 15:30:33 -0500

    appendix Idealized Methodology and Survey added

---
+++ scripts/06-model.R
@@ -0,0 +1,17 @@
+#### Preamble ####
+# Purpose: 
+# Author: Yingke He, Ziheng Zhong
+# Date: 31 October 2024
+# Contact: ziheng.zhong@mail.utoronto.ca
+# License: MIT
+
+
+
+#### Workspace setup ####
+
+
+
+
+#### Modeling ####
+
+
